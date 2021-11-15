13fdcf265e7ce7de577a1c953abb314f7a32a195
ktichon <ktichon@icould.com>
2021-11-15 13:51:49 -0600

    Fixed BookInOrder references issue

--- db/migrate/20211115194511_create_book_in_orders.rb
+++ db/migrate/20211115194511_create_book_in_orders.rb
@@ -2,12 +2,9 @@ class CreateBookInOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :book_in_orders do |t|
       t.decimal :sold_price
-      t. :amout
-      t.string :order
-      t.string :references
-      t.string :book
-      t.string :references
-
+      t.decimal :amount
+      t.references :order, null: false, foreign_key: true
+      t.references :book, null: false, foreign_key: true
       t.timestamps
     end
   end
---
+++ db/schema.rb
@@ -0,0 +1,94 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_15_194511) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "info"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "book_genres", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_genres_on_book_id"
+    t.index ["genre_id"], name: "index_book_genres_on_genre_id"
+  end
+
+  create_table "book_in_orders", force: :cascade do |t|
+    t.decimal "sold_price"
+    t.decimal "amount"
+    t.integer "order_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_book_in_orders_on_book_id"
+    t.index ["order_id"], name: "index_book_in_orders_on_order_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "cost"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "address"
+    t.string "postal_code"
+    t.integer "provence_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["provence_id"], name: "index_customers_on_provence_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.decimal "GST"
+    t.decimal "HST"
+    t.decimal "total"
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "abbrev"
+    t.string "name"
+    t.decimal "PST"
+    t.decimal "HST"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "book_genres", "books"
+  add_foreign_key "book_genres", "genres"
+  add_foreign_key "book_in_orders", "books"
+  add_foreign_key "book_in_orders", "orders"
+  add_foreign_key "books", "authors"
+  add_foreign_key "customers", "provences"
+  add_foreign_key "orders", "customers"
+end
