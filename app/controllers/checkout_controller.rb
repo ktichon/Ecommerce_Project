0f120d210ea18cfa5eae27568eb55e5d9d76db6e
ktichon <ktichon@icould.com>
2021-12-04 21:34:45 -0600

    Stripe format.js isn't working, however user data is auto added to form now

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,4 +1,13 @@
 class CheckoutController < ApplicationController
+
+  def index
+    if current_user
+      province = Province.find(current_user.province_id)
+      @povince_name = province.name
+    end
+
+  end
+
   def create
     province = Province.find(params[:province_id])
     customer = province.customers.create(address: params[:address], postal_code: params[:postal], email: params[:email])
@@ -9,13 +18,33 @@ class CheckoutController < ApplicationController
     end
     total_after_tax = cart[0] + cart[0] * province.PST + cart[0] * province.GST + cart[0] * province.HST
     order = customer.orders.create(status: "New", PST: province.PST, GST: province.GST, HST: province.HST, total: total_after_tax)
+    line_items = []
     cart[1].each do |bookInCart|
       BookInOrder.create(order: order, book: bookInCart[0], sold_price: bookInCart[0].cost, amount: bookInCart[1])
+      line_items << {name: bookInCart[0].name, description: bookInCart[0].description, amount:  (bookInCart[0].cost * 100).to_i, quantity: bookInCart[1], currency: "cad" }
     end
-    # respond_to do |format|
-    #   format.js
-    # end
-    redirect_back fallback_location: root_path
+    if province.GST > 0
+      line_items << {name: "GST", description: "Goods and Services Tax", amount: (cart[0] * province.GST * 100).to_i , quantity: 1, currency: "cad" }
+    end
+    if province.PST > 0
+      line_items << {name: "PST", description: "Provincial Sales Tax", amount: (cart[0] * province.PST * 100).to_i , quantity: 1, currency: "cad" }
+    end
+    if province.HST > 0
+      line_items << {name: "PST", description: "Harmonized Sales Tax", amount: (cart[0] * province.HST * 100).to_i , quantity: 1, currency: "cad" }
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      payment_method_types: ["card"],
+      success_url:          checkout_success_url,
+      cancel_url:           checkout_cancel_url,
+      line_items:           line_items,
+      customer_email:       params[:email]
+    )
+
+    respond_to do |format|
+      format.js # render app/views/checkout/create.js.erb
+    end
+
 
     #connects to strip
 
